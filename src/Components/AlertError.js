4b0fd9af061c56f8865269fcc70e168bfa803acf
Walid Siala <wsiala4@gmail.com>
2023-07-02 16:34:51 +0100

    Change Error Sentence

--- src/Components/AlertError.js
+++ src/Components/AlertError.js
@@ -3,7 +3,7 @@ import React from "react";
 const AlertError = () => {
   return (
     <div class="alert alert-danger" role="alert">
-      Verify the medias -- Check it out !
+      Please verify the medias !
     </div>
   );
 };
